f50db0814a53ad42454acb8a0aed08cc620fdc25
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-07 18:54:57 +0200

    sett "is not existing" på bakgrunn av tallet 0

--- verify/verify.sh
+++ verify/verify.sh
@@ -6,25 +6,26 @@ set -e
 # Følgende skjer i dette skriptet (krever bla. miljøvariabelen IMAGE):
 # 1) henter manifest, samt  for miljøvariabelen IMAGE
 # 2) teller antall ganger teksten "HTTP/1.1 404" kommer i head requesten etter manifestet
-# 3) hvis antallet er 1, så er det et "is not existing" satt til true, hvis ikke false
+# 3) hvis antall av HTTP/1.1 404 er 0, så blir "is not existing" satt false, hvis ikke true
 #
 ############################################
 
 REQUEST=$(curl --head --request GET "https://$IMAGE/manifests/$GITHUB_SHA -u $GITHUB_ACTOR:$GITHUB_TOKEN")
 echo "
->---
 Github head request:
-$REQUEST>---
-"
+----
+$REQUEST"
+
 NOT_FOUND="HTTP/1.1\ 404"
 NF_COUNT=$(echo "$REQUEST" | grep -c "$NOT_FOUND"  || true)
+
 echo "
 Found $NF_COUNT mentions of $NOT_FOUND"
 
-if [ "$NF_COUNT" -eq 1 ]
+if [ "$NF_COUNT" -eq 0 ]
   then
-    echo ::set-output name=is_not_existing::true
+    echo ::set-output name=is_not_existing::false
     exit 0;
 fi
 
-echo ::set-output name=is_not_existing::false
+echo ::set-output name=is_not_existing::true
