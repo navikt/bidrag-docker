e56936725b172ccc5078f4d51e628d889d7f6d76
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-08 13:52:24 +0200

    io and curl statement
    * input: image name - text
    * output: not_found - true/false (text)
    * changed behaviour based on http codes: 404 and 200

--- exists/exists.sh
+++ exists/exists.sh
@@ -3,29 +3,41 @@ set -e
 
 ############################################
 #
-# Følgende skjer i dette skriptet (krever bla. miljøvariabelen IMAGE):
-# 1) henter manifest, samt  for miljøvariabelen IMAGE
+# Følgende skjer i dette skriptet
+# 1) henter headere til en GET request etter docker-manifest på github
 # 2) teller antall ganger teksten "HTTP/1.1 404" kommer i head requesten etter manifestet
-# 3) hvis antall av HTTP/1.1 404 er 0, så blir "is not existing" satt false, hvis ikke true
+# 3a) når antall av "HTTP/1.1 404" er 0, så blir "not found" satt false
+#     - hvis antall av "HTTP/1.0 200" ikke er 0 ellers så feiler action
+# 4) når forrige tester ikke slår til så er "is not found" true
 #
 ############################################
 
-REQUEST=$(curl --head --request GET "https://$IMAGE/manifests/$GITHUB_SHA" -u "$GITHUB_ACTOR:$GITHUB_TOKEN")
+IMAGE_NAME=$1
+
+REQUEST=$(curl --head --request GET "https://docker.pkg.github.com/v2/$GITHUB_REPOSITORY/$IMAGE_NAME/manifests/$GITHUB_SHA" -u "$GITHUB_ACTOR:$GITHUB_TOKEN")
 echo "
 Github head request:
 ----
 $REQUEST"
 
 NOT_FOUND="HTTP/1.1\ 404"
-NF_COUNT=$(echo "$REQUEST" | grep -c "$NOT_FOUND"  || true)
+NF_COUNT=$(echo "$REQUEST" | grep -c "$NOT_FOUND" || true)
 
 echo "
 Found $NF_COUNT mentions of $NOT_FOUND"
 
-if [ "$NF_COUNT" -eq 0 ]
-  then
-    echo ::set-output name=is_not_existing::false
-    exit 0;
+if [ "$NF_COUNT" -eq 0 ]; then
+  SUCCESS="HTTP/1.1\ 200"
+  SUCCESS_COUNT=$(echo "$REQUEST" | grep -c "$SUCCESS" || true)
+
+  if [ "$SUCCESS_COUNT" -eq 0 ]; then
+    echo ::error not a successful http request for headers
+    exit 1;
+  fi
+
+  echo a successful http request for headers
+  echo ::set-output name=not_found::false
+  exit 0;
 fi
 
-echo ::set-output name=is_not_existing::true
+echo ::set-output name=not_found::true
