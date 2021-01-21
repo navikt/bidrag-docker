aafb08f686641f2254805ecc398334732a9057ef
s148719 <jan.kjetil.skar@nav.no>
2021-01-21 19:00:16 +0100

    http/2.0 => http/2

--- exists/exists.sh
+++ exists/exists.sh
@@ -5,9 +5,9 @@ set -e
 #
 # Følgende skjer i dette skriptet
 # 1) henter headere til en GET request etter docker-manifest på github
-# 2) teller antall ganger teksten "HTTP/2.0 404" kommer i head requesten etter manifestet
-# 3a) når antall av "HTTP/2.0 404" er 0, så blir "not found" satt false
-#     - hvis antall av "HTTP/2.0 401" er 1, så blir "not found" satt til false
+# 2) teller antall ganger teksten "HTTP/2 404" kommer i head requesten etter manifestet
+# 3a) når antall av "HTTP/2 404" er 0, så blir "not found" satt false
+#     - hvis antall av "HTTP/2 401" er 1, så blir "not found" satt til false
 #     - hvis antall av "HTTP/1.0 200" ikke er 0 ellers så feiler action
 # 4) når forrige tester ikke slår til så er "is not found" true
 #
@@ -22,14 +22,14 @@ Headere i respons fra $URL:
 ----
 $REQUEST"
 
-NOT_FOUND="HTTP/2.0\ 404"
+NOT_FOUND="HTTP/2\ 404"
 NF_COUNT=$(echo "$REQUEST" | grep -c "$NOT_FOUND" || true)
 
 echo "
 Found $NF_COUNT mentions of $NOT_FOUND"
 
 if [ "$NF_COUNT" -eq 0 ]; then
-  UNAUTHORIZED="HTTP/2.0\ 401"
+  UNAUTHORIZED="HTTP/2\ 401"
   UNAUTHORIZED_COUNT=$(echo "$REQUEST" | grep -c "$UNAUTHORIZED" || true)
 
   if [ "$UNAUTHORIZED_COUNT" -eq 1 ]; then
@@ -38,7 +38,7 @@ if [ "$NF_COUNT" -eq 0 ]; then
     exit 0;
   fi
 
-  SUCCESS="HTTP/2.0\ 200"
+  SUCCESS="HTTP/2\ 200"
   SUCCESS_COUNT=$(echo "$REQUEST" | grep -c "$SUCCESS" || true)
 
   if [ "$SUCCESS_COUNT" -eq 0 ]; then
