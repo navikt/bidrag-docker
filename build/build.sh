2f418854ad117c9eb3b430da74c2cd3a1ae3f8b7
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-04-21 13:58:11 +0200

    podman vs docker

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,20 @@ set -e
 
 ############################################
 #
-# Følgende skjer i dette skriptet (krever bla. miljøvariabelen IMAGE):
-# 1) bygger et docker image fra miljøvariabelen IMAGE (bruker podman)
-# 2) bruker GITHUB_TOKEN til docker login til repoet som bygges
-# 3) pusher image til github registry
+# Følgende skjer i dette skriptet (krever bla. miljøvariabelen IMAGE, GITHUB_REPOSITORY og GITHUB_TOKEN):
+# 1) sjekker om vi skal bruke podman eller docker
+# 2) bygger et docker image fra miljøvariabelen IMAGE
+# 3) bruker GITHUB_TOKEN til docker login til repoet som bygges
+# 4) pusher image til github registry
 #
 ############################################
 
-sudo podman build --tag ${IMAGE} .
-sudo podman login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p ${GITHUB_TOKEN}
-sudo podman push ${IMAGE}
+if type podman  > /dev/null; then
+  sudo podman build --tag ${IMAGE} .
+  sudo podman login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p ${GITHUB_TOKEN}
+  sudo podman push ${IMAGE}
+else
+  docker build --tag ${IMAGE} .
+  docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p ${GITHUB_TOKEN}
+  docker push ${IMAGE}
+fi
