379aa28fce761be8807b47ee858cb0d2d1265f43
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-04-21 12:47:41 +0200

    finding which protocol to use (HTTP/2 or HTTP/1.1)

--- exists/exists.sh
+++ exists/exists.sh
@@ -5,10 +5,10 @@ set -e
 #
 # Følgende skjer i dette skriptet
 # 1) henter headere til en GET request etter docker-manifest på github
-# 2) teller antall ganger teksten "HTTP/2 404" kommer i head requesten etter manifestet
-# 3a) når antall av "HTTP/2 404" er 0, så blir "not found" satt false
-#     - hvis antall av "HTTP/2 401" er 1, så blir "not found" satt til false
-#     - hvis antall av "HTTP/1.0 200" ikke er 0 ellers så feiler action
+# 2) teller antall ganger teksten "HTTP/* 404" kommer i head requesten etter manifestet
+# 3) når antall av "HTTP/* 404" er 0, så blir "not found" satt lik:
+#     - antall av "HTTP/* 401" er 1, så blir "not found" satt til false
+#     - antall av "HTTP/*200" ikke er 0 ellers så feiler action
 # 4) når forrige tester ikke slår til så er "is not found" true
 #
 ############################################
@@ -22,14 +22,22 @@ Headere i respons fra $URL:
 ----
 $REQUEST"
 
-NOT_FOUND="HTTP/2\ 404"
+HTTP="HTTP/2"
+NO_OF_HTTP2=$(echo echo "$REQUEST" | grep -c "HTTP/2" || true)
+
+if [ "$NO_OF_HTTP2" -eq 0 ]; then
+  echo unable to find protocol HTTP/2, using HTTP/1.1...
+  HTTP="HTTP/1.1"
+fi
+
+NOT_FOUND="$HTTP\ 404"
 NF_COUNT=$(echo "$REQUEST" | grep -c "$NOT_FOUND" || true)
 
 echo "
 Found $NF_COUNT mentions of $NOT_FOUND"
 
 if [ "$NF_COUNT" -eq 0 ]; then
-  UNAUTHORIZED="HTTP/2\ 401"
+  UNAUTHORIZED="$HTTP\ 401"
   UNAUTHORIZED_COUNT=$(echo "$REQUEST" | grep -c "$UNAUTHORIZED" || true)
 
   if [ "$UNAUTHORIZED_COUNT" -eq 1 ]; then
@@ -38,11 +46,11 @@ if [ "$NF_COUNT" -eq 0 ]; then
     exit 0;
   fi
 
-  SUCCESS="HTTP/2\ 200"
+  SUCCESS="$HTTP\ 200"
   SUCCESS_COUNT=$(echo "$REQUEST" | grep -c "$SUCCESS" || true)
 
   if [ "$SUCCESS_COUNT" -eq 0 ]; then
-    echo ::error not a successful http request for headers
+    echo ::error "not a successful http request ($HTTP) according to headers"
     exit 1;
   fi
 
